1665e52542e616a5674d9b4e2dfeafee8320c33d
ashan-shashika <ashanshashikasilva@gmail.com>
2018-04-04 19:48:49 +0530

    delete mutations

---
+++ graphql/mutation/user/addUser.mutation.js
@@ -0,0 +1,19 @@
+import models from '../../../models/index.js';
+import UserType from '../../types/user.type.js';
+import UserInput from "../../input/user.input.js"
+
+export default {
+  type: UserType,
+  args: {
+      user: {
+          type: UserInput
+      }
+  },
+  resolve (source, args) {
+    return models.user.create({
+      firstName: args.user.firstName,
+      lastName: args.user.lastName,
+      email: args.user.email.toLowerCase()
+    });
+  }
+}
